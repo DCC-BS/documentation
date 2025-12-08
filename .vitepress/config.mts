a35b24b24b5a652e8e0e949bbf47cb8f760c2f26
Tobias Bollinger <tobias.bollinger@bs.ch>
2025-12-08 11:36:53 +0100

    Add Nuxt + TypeScript coding standards

--- .vitepress/config.mts
+++ .vitepress/config.mts
@@ -1,29 +1,24 @@
-import { defineConfig } from 'vitepress'
 import { withMermaid } from "vitepress-plugin-mermaid";
 
 // https://vitepress.dev/reference/site-config
 export default withMermaid({
-  title: "DCC Dev Guidelines",
-  description: "Guidelines for the DCC Developers",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    logo: '/imgs/logo.png',
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Team', link: '/team' }
-    ],
+	title: "DCC Dev Guidelines",
+	description: "Guidelines for the DCC Developers",
+	themeConfig: {
+		// https://vitepress.dev/reference/default-theme-config
+		logo: "/imgs/logo.png",
+		nav: [
+			{ text: "Home", link: "/" },
+			{ text: "Team", link: "/team" },
+		],
 
-    sidebar: [
-      {
-        text: 'Docker',
-        items: [
-          { text: 'Docker', link: '/docker' }
-        ]
-      }
-    ],
+		sidebar: [
+			{
+				text: "Docker",
+				items: [{ text: "Docker", link: "/docker" }],
+			},
+		],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/dcc-bs' },
-    ]
-  }
-})
+		socialLinks: [{ icon: "github", link: "https://github.com/dcc-bs" }],
+	},
+});
--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,70 +1,60 @@
-import { defineConfig } from 'vitepress'
 import { withMermaid } from "vitepress-plugin-mermaid";
-
 
 // https://vitepress.dev/reference/site-config
 export default withMermaid({
-  srcDir: "markdown",
-  base: process.env.NODE_ENV === 'production' ? '/documentation/' : '/',
-  title: "DCC Dev Guidelines",
-  description: "Guidelines for the DCC Developers",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    logo: 'imgs/logo.png',
-    editLink: {
-      pattern: 'https://github.com/dcc-bs/documentation/edit/main/markdown/:path',
-    },
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Team', link: '/team' },
-    ],
-    footer: {
-      message: "Developed with ❤️ by the DCC. Documentation released under the MIT License.",
-      copyright: "Copyright © 2025 DCC",
-    },
+	srcDir: "markdown",
+	base: process.env.NODE_ENV === "production" ? "/documentation/" : "/",
+	title: "DCC Dev Guidelines",
+	description: "Guidelines for the DCC Developers",
+	themeConfig: {
+		// https://vitepress.dev/reference/default-theme-config
+		logo: "imgs/logo.png",
+		editLink: {
+			pattern:
+				"https://github.com/dcc-bs/documentation/edit/main/markdown/:path",
+		},
+		nav: [
+			{ text: "Home", link: "/" },
+			{ text: "Team", link: "/team" },
+		],
+		footer: {
+			message:
+				"Developed with ❤️ by the DCC. Documentation released under the MIT License.",
+			copyright: "Copyright © 2025 DCC",
+		},
 
-    search: {
-      provider: 'local',
-    },
+		search: {
+			provider: "local",
+		},
 
-    sidebar: [
-      {
-        text: 'Dev Setup',
-        items: [
-          { text: 'Dev Setup', link: '/dev-setup' }
-        ]
-      },
-      {
-        text: 'Docker',
-        items: [
-          { text: 'Docker', link: '/docker' }
-        ]
-      },
-      {
-        text: 'Coding',
-        items: [
-          { text: 'Coding', link: '/general' },
-          { text: 'Python', link: '/python' },
-          { text: 'Nuxt / Vue', link: '/nuxt' }
-        ]
-      },
-      {
-        text: 'Git / GitHub / CI/CD',
-        items: [
-          { text: 'Git / GitHub / CI/CD', link: '/git' }
-        ]
-      },
-      {
-        text: 'Team',
-        items: [
-          { text: 'Team', link: '/team' }
-        ]
-      }
-    ],
+		sidebar: [
+			{
+				text: "Dev Setup",
+				items: [{ text: "Dev Setup", link: "/dev-setup" }],
+			},
+			{
+				text: "Docker",
+				items: [{ text: "Docker", link: "/docker" }],
+			},
+			{
+				text: "Coding",
+				items: [
+					{ text: "Coding", link: "/general" },
+					{ text: "Python", link: "/python" },
+					{ text: "Nuxt / Vue", link: "/nuxt" },
+				],
+			},
+			{
+				text: "Git / GitHub / CI/CD",
+				items: [{ text: "Git / GitHub / CI/CD", link: "/git" }],
+			},
+			{
+				text: "Team",
+				items: [{ text: "Team", link: "/team" }],
+			},
+		],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/dcc-bs' },
-    ]
-  },
-  lastUpdated: true
-})
+		socialLinks: [{ icon: "github", link: "https://github.com/dcc-bs" }],
+	},
+	lastUpdated: true,
+});
