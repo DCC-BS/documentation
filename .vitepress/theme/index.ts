d56325e3d3574e18030255fa6e40c8802d1231e8
Tobias Bollinger <tobias.bollinger@gmail.com>
2025-12-18 17:21:21 +0100

    Remove Playwright config and clean theme imports

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,12 +1,11 @@
+import "./main.css";
+
 import ui from "@nuxt/ui/vue-plugin";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import { createI18n } from "vue-i18n";
 import en from "../../i18n/en.json";
 import Layout from "./Layout.vue";
-
-import "@dcc-bs/common-ui.bs.js/runtime/assets/kantonbs/colors.css";
-import "./main.css";
 
 export default {
     extends: DefaultTheme,
